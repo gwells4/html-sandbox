8c1aa60e782c6a6122b4ebbd09766936b4013cfe
Grant Wells <gwells4@cnm.edu>
2016-10-11 13:39:37 -0600

    Added alt text for first sandbox image.

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 			<!-- Begin section 1 -->
 			<section>
 				<p>Jean shorts biodiesel coloring book, skateboard farm-to-table tofu kombucha put a bird on it sartorial quinoa vexillologist authentic kitsch. Lo-fi pok pok mumblecore hell of put a bird on it, ramps squid before they sold out beard bushwick. Tacos hoodie tousled tumblr poutine. Hella DIY austin twee yr. Lomo kombucha tilde mlkshk forage flexitarian. Pok pok cray williamsburg direct trade unicorn vinyl viral. Knausgaard chia helvetica post-ironic, meggings schlitz lomo.</p>
-				<img src="img/sandbox-image-1.jpg" alt="">
+				<img src="img/sandbox-image-1.jpg" alt="First Sandbox Image">
 			</section>
 			<section>
 
