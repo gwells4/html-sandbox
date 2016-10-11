b0932a1bd5905c34557bfefbadac84c4eda94004
Grant Wells <gwells4@cnm.edu>
2016-10-11 13:29:40 -0600

    Added first sandbox image.

--- index.php
+++ index.php
@@ -1,11 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<meta charset="utf-8" />
+		<meta charset="utf-8">
 		<title>Repository 2: HTML Sandbox</title>
 	</head>
 	<body>
 		<h1>Hello World!</h1>
 		<p>This is the place to practice HTML.</p>
+
+		<!-- This is the header -->
+		<header>
+			<h1>Hi! This is my page.</h1>
+		</header>
+
+		<!-- This is the main page content. -->
+		<main>
+
+			<!-- Begin section 1 -->
+			<section>
+				<p>Jean shorts biodiesel coloring book, skateboard farm-to-table tofu kombucha put a bird on it sartorial quinoa vexillologist authentic kitsch. Lo-fi pok pok mumblecore hell of put a bird on it, ramps squid before they sold out beard bushwick. Tacos hoodie tousled tumblr poutine. Hella DIY austin twee yr. Lomo kombucha tilde mlkshk forage flexitarian. Pok pok cray williamsburg direct trade unicorn vinyl viral. Knausgaard chia helvetica post-ironic, meggings schlitz lomo.</p>
+				<img src="" alt="">
+			</section>
+			<section>
+
+			</section>
+			<section>
+
+			</section>
+			<ul>
+
+			</ul>
+			<table>
+
+			</table>
+		</main>
+
 	</body>
 </html>
