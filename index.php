9577460fb6e45d1ca7e72e49370d190a847202ea
Grant Wells <gwells4@cnm.edu>
2016-10-11 13:40:51 -0600

    Deleted "Hello World!" text.

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+
 	<head>
 		<meta charset="utf-8">
 		<title>Repository 2: HTML Sandbox</title>
 	</head>
+
 	<body>
-		<h1>Hello World!</h1>
-		<p>This is the place to practice HTML.</p>
 
 		<!-- This is the header -->
 		<header>
@@ -21,6 +21,8 @@
 				<p>Jean shorts biodiesel coloring book, skateboard farm-to-table tofu kombucha put a bird on it sartorial quinoa vexillologist authentic kitsch. Lo-fi pok pok mumblecore hell of put a bird on it, ramps squid before they sold out beard bushwick. Tacos hoodie tousled tumblr poutine. Hella DIY austin twee yr. Lomo kombucha tilde mlkshk forage flexitarian. Pok pok cray williamsburg direct trade unicorn vinyl viral. Knausgaard chia helvetica post-ironic, meggings schlitz lomo.</p>
 				<img src="img/sandbox-image-1.jpg" alt="First Sandbox Image">
 			</section>
+
+			<!-- Begin section 2 -->
 			<section>
 
 			</section>
